f5ee06ef4f24f04e6de5b39fbbf763ed1b2413bd
Jay Deng <jed326@Jays-MacBook-Pro.local>
2019-12-03 19:06:02 -0500

    Boilerplate, initial deployment to S3

---
+++ src/components/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactPageScroller from "react-page-scroller";
+
+import Landing from "./Landing";
+
+import "../styles/App.css";
+
+type PageState = {
+    page: Number;
+};
+
+// References: https://github.com/VikLiegostaiev/react-page-scroller/blob/master/demo/src/FullPage.js
+
+export default class App extends React.Component<{}, PageState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = { page: 0 };
+    }
+
+    handlePageChange = (nextPage: Number) => {
+        this.setState({ page: nextPage });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <ReactPageScroller>
+                    <Landing></Landing>
+                    <div>About Me</div>
+                    <div>Work Experience</div>
+                    <div>Projects</div>
+                </ReactPageScroller>
+            </div>
+        );
+    }
+}
